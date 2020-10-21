bcdec00d2a6a40c2113a79d238e4c4010e914189
Yuri Trondin <lopata@2ch.hk>
2020-10-21 15:49:47 +0500

    first commit

---
+++ src/components/Header.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+const Header = () => {
+  return (
+    <header className="header">
+      <img
+        src="https://www.clubsportive.nl/wp-content/uploads/2018/12/logo-example-300x150.jpg"
+        alt="logo"
+      />
+    </header>
+  );
+};
+export default Header;
---
+++ src/components/Profile.jsx
@@ -0,0 +1,22 @@
+import React from "react";
+
+const Profile = () => {
+  return (
+    <main className="content">
+      <img
+        src="https://cdn.pixabay.com/photo/2019/10/29/10/07/man-4586576_960_720.jpg"
+        alt="wallpaper"
+      />
+      <div>ava+desc</div>
+      <div>
+        mypost
+        <div>newpost</div>
+        <div>
+          <div>post1</div>
+          <div>post2</div>
+        </div>
+      </div>
+    </main>
+  );
+};
+export default Profile;
