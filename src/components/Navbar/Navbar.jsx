3977045940aa1dd7738e8b846ed6abb8e5b14b6f
Yuri Trondin <lopata@2ch.hk>
2020-10-22 14:59:43 +0500

    добавил страницу диалогов

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -17,6 +17,11 @@ const Navbar = () => {
         </li>
         <li>
           <a className={classes.item} href="#">
+            Messages
+          </a>
+        </li>
+        <li>
+          <a className={classes.item} href="#">
             Music
           </a>
         </li>
--- src/components/Profile/Profile.jsx
+++ src/components/Profile/Profile.jsx
@@ -3,7 +3,7 @@ import MyPosts from "./MyPosts/MyPosts";
 import classes from "./Profile.module.css";
 const Profile = () => {
   return (
-    <main className="content">
+    <main>
       <img
         src="https://cdn.pixabay.com/photo/2019/10/29/10/07/man-4586576_960_720.jpg"
         alt="wallpaper"
