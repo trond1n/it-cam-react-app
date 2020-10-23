ec0973c623f362dd994686df1785fb9e51c74898
Yuri Trondin <lopata@2ch.hk>
2020-10-23 09:54:15 +0500

    начал верстку диалогов

--- src/components/Dialogs/Dialogs.jsx
+++ src/components/Dialogs/Dialogs.jsx
@@ -1,9 +1,21 @@
 import React from "react";
 import classes from "./Dialogs.module.css";
 const Dialogs = (props) => {
-  return(
-    <div>
-      Dialogs
+  return (
+    <div className={classes.dialogs}>
+      <div className={classes.dialogs__items}>
+        <div className={classes.dialog}>Dima</div>
+        <div className={classes.dialog + " " + classes.active}>Andrew</div>
+        <div className={classes.dialog}>Lena</div>
+        <div className={classes.dialog}>Alex</div>
+        <div className={classes.dialog}>Max</div>
+        <div className={classes.dialog}>Ivan</div>
+      </div>
+      <div className={classes.messages}>
+        <div className={classes.message}>Hello</div>
+        <div className={classes.message}>H r U?</div>
+        <div className={classes.message}>Nice code</div>
+      </div>
     </div>
   );
 };
