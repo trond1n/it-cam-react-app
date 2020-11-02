ae12f6ff5781d968634ef5542bd117e68afe42a6
Yuri Trondin <lopata@2ch.hk>
2020-11-02 20:53:54 +0500

    переписал юзеров на АПИ и классовую компоненту

--- src/components/Users/Users.jsx
+++ src/components/Users/Users.jsx
@@ -1,92 +1,132 @@
 import React from "react";
 import styles from "./users.module.css";
+import * as axios from "axios";
 
-const Users = (props) => {
-  if (props.users.length === 0) {
-    props.setUsers([
-      {
-        id: 1,
-        photoUrl:
-          "https://sun3-13.userapi.com/impf/c855536/v855536760/a12f6/z0432RfuLxE.jpg?size=100x0&quality=88&crop=125,122,479,479&sign=fee2837fa1f9c5569b91cee34e4bbf7f&ava=1",
-        followed: true,
-        fullName: "Dmitry K.",
-        status: "Have a good day",
-        location: { city: "Moscow", country: "Russia" },
-      },
-      {
-        id: 2,
-        photoUrl:
-          "https://sun3-12.userapi.com/impf/c845324/v845324527/28081/SaBJZMPYsfg.jpg?size=100x0&quality=88&crop=88,0,379,379&sign=4c0ce4df0a26430c48760c77b4cd3141&ava=1",
-        followed: false,
-        fullName: "Ura A.",
-        status: "Nice day",
-        location: { city: "Astana", country: "Kazakhstan" },
-      },
-      {
-        id: 3,
-        photoUrl:
-          "https://sun3-12.userapi.com/impg/c857632/v857632427/200918/l00ii3Ty4AU.jpg?size=100x0&quality=88&crop=0,174,1763,1763&sign=1004370ca497ac79f32f7d6b2bdcc32a&ava=1",
-        followed: false,
-        fullName: "Anna A",
-        status: "Hello, world",
-        location: { city: "Kiyv", country: "Ukraine" },
-      },
-      {
-        id: 4,
-        photoUrl:
-          "https://sun3-13.userapi.com/impg/8b8rohCw9ZBsacKrv4tKl-T4CV7JCkyFsOm16w/7ONzLA5_KQM.jpg?size=100x0&quality=88&crop=96,96,768,768&sign=2e5c6fe18010e198f56e0ff29b187d52&ava=1",
-        followed: true,
-        fullName: "Lena A",
-        status: "Good Morning",
-        location: { city: "Minsk", country: "Belarus" },
-      },
-    ]);
+//функциональная компонента
+// const Users = (props) => {
+//   let getUsers = () => {
+//     if (props.users.length === 0) {
+//       axios
+//         .get("https://social-network.samuraijs.com/api/1.0/users")
+//         .then((response) => {
+//           props.setUsers(response.data.items);
+//         });
+//     }
+//   };
+
+//   return (
+//     <div>
+//       <button onClick={getUsers}>Get Users</button>
+//       {props.users.map((u) => (
+//         <div key={u.id}>
+//           <span>
+//             <div>
+//               <img
+//                 src={
+//                   u.photos.small != null
+//                     ? u.photos.small
+//                     : "https://vk.com/images/camera_400.png?ava=1"
+//                 }
+//                 alt="фото пользователя"
+//                 className={styles.user__photo}
+//               />{" "}
+//             </div>
+//             <div>
+//               {u.followed ? (
+//                 <button
+//                   onClick={() => {
+//                     props.unfollow(u.id);
+//                   }}
+//                 >
+//                   Unfollow
+//                 </button>
+//               ) : (
+//                 <button
+//                   onClick={() => {
+//                     props.follow(u.id);
+//                   }}
+//                 >
+//                   Follow
+//                 </button>
+//               )}
+//             </div>
+//           </span>
+//           <span>
+//             <div>{u.name}</div>
+//             <div>{u.status}</div>
+//           </span>
+//           <span>
+//             <div>{"Страна"}</div>
+//             <div>{"Город"}</div>
+//           </span>
+//         </div>
+//       ))}
+//     </div>
+//   );
+// };
+
+//классовая компонента
+class Users extends React.Component {
+  constructor(props) {
+    super(props);
+    if (this.props.users.length === 0) {
+      axios
+        .get("https://social-network.samuraijs.com/api/1.0/users")
+        .then((response) => {
+          this.props.setUsers(response.data.items);
+        });
+    }
   }
 
-  return (
-    <div>
-      {props.users.map((u) => (
-        <div key={u.id}>
-          <span>
-            <div>
-              <img
-                src={u.photoUrl}
-                alt="фото пользователя"
-                className={styles.user__photo}
-              />{" "}
-            </div>
-            <div>
-              {u.followed ? (
-                <button
-                  onClick={() => {
-                    props.unfollow(u.id);
-                  }}
-                >
-                  Unfollow
-                </button>
-              ) : (
-                <button
-                  onClick={() => {
-                    props.follow(u.id);
-                  }}
-                >
-                  Follow
-                </button>
-              )}
-            </div>
-          </span>
-          <span>
-            <div>{u.fullName}</div>
-            <div>{u.status}</div>
-          </span>
-          <span>
-            <div>{u.location.country}</div>
-            <div>{u.location.city}</div>
-          </span>
-        </div>
-      ))}
-    </div>
-  );
-};
-
+  render() {
+    return (
+      <div>
+        {this.props.users.map((u) => (
+          <div key={u.id}>
+            <span>
+              <div>
+                <img
+                  src={
+                    u.photos.small != null
+                      ? u.photos.small
+                      : "https://vk.com/images/camera_400.png?ava=1"
+                  }
+                  alt="фото пользователя"
+                  className={styles.user__photo}
+                />{" "}
+              </div>
+              <div>
+                {u.followed ? (
+                  <button
+                    onClick={() => {
+                      this.props.unfollow(u.id);
+                    }}
+                  >
+                    Unfollow
+                  </button>
+                ) : (
+                  <button
+                    onClick={() => {
+                      this.props.follow(u.id);
+                    }}
+                  >
+                    Follow
+                  </button>
+                )}
+              </div>
+            </span>
+            <span>
+              <div>{u.name}</div>
+              <div>{u.status}</div>
+            </span>
+            <span>
+              <div>{"Страна"}</div>
+              <div>{"Город"}</div>
+            </span>
+          </div>
+        ))}
+      </div>
+    );
+  }
+}
 export default Users;
